aaa993dd34e467bca4b4530393175ebfda10be69
Davidnino21 <davidnino_21@yahoo.com>
2025-02-06 15:29:50 -0800

    initial commit. Added page structure

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="title">
+        <header>
+          <h1>Music Finder</h1>
+        </header>
+      </div>
+      <div className="search-results">
+          <input type="text" />
+          <button>Search</button>
+      </div>
+      <div className="main-content">
+          <div className="music-cards">
+            <div className="card">
+              <h3>Song Title</h3>
+              <h3>Artist Name</h3>
+              <button>Play</button>
+            </div>
+            <div className="card">
+              <h3>Song Title</h3>
+              <h3>Artist Name</h3>
+              <button>Play</button>
+            </div>
+            <div className="card">
+              <h3>Song Title</h3>
+              <h3>Artist Name</h3>
+              <button>Play</button>
+            </div>
+            <div className="card">
+              <h3>Song Title</h3>
+              <h3>Artist Name</h3>
+              <button>Play</button>
+            </div>
+            <div className="card">
+              <h3>Song Title</h3>
+              <h3>Artist Name</h3>
+              <button>Play</button>
+            </div>
+            <div className="card">
+              <h3>Song Title</h3>
+              <h3>Artist Name</h3>
+              <button>Play</button>
+            </div>
+          </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
