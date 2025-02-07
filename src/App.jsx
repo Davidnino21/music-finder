6e2a69784f5f3af897691e151a08db2514a1f263
Davidnino21 <davidnino_21@yahoo.com>
2025-02-07 13:37:54 -0800

    updated styling on cards

--- src/App.jsx
+++ src/App.jsx
@@ -10,52 +10,42 @@ function App() {
         </header>
       </div>
       <div className="search-results">
-          <input type="text" />
-          <button id="search-btn">Search</button>
+        <input type="text" />
+        <button id="search-btn">Search</button>
       </div>
       <div className="main-content">
-          <div className="music-cards">
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
-            <div className="card">
-              <h3>Song Title</h3>
-              <h3>Artist Name</h3>
-              <button>Play</button>
-            </div>
+        <div className="music-cards">
+          <div className="card">
+            <h3>Song Title</h3>
+            <h3>Artist Name</h3>
+            <button>Play</button>
           </div>
+          <div className="card">
+            <h3>Song Title</h3>
+            <h3>Artist Name</h3>
+            <button>Play</button>
+          </div>
+          <div className="card">
+            <h3>Song Title</h3>
+            <h3>Artist Name</h3>
+            <button>Play</button>
+          </div>
+          <div className="card">
+            <h3>Song Title</h3>
+            <h3>Artist Name</h3>
+            <button>Play</button>
+          </div>
+          <div className="card">
+            <h3>Song Title</h3>
+            <h3>Artist Name</h3>
+            <button>Play</button>
+          </div>
+          <div className="card">
+            <h3>Song Title</h3>
+            <h3>Artist Name</h3>
+            <button>Play</button>
+          </div>
+        </div>
       </div>
     </>
   );
